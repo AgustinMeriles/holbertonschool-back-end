8fc8870d31e0f4856023ba0fd88857ebe388b929
agustinmeriles <agustinmeriles@hotmail.com>
2023-04-02 16:06:05 -0300

    2-export_to_JSON.py

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Shebang"""
+import requests
+import json
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user = requests.get(user_url).json()
+
+    todos_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'
+                 .format(user_id))
+    todos = requests.get(todos_url).json()
+
+    completed_tasks = [task for task in todos if task.get('completed') is True]
+
+    data = {user_id: [{"task": task.get('title'), "completed":
+            task.get('completed'), "username": user.get('username')}
+            for task in completed_tasks]}
+
+    with open(f"{user_id}.json", "w") as outfile:
+        json.dump(data, outfile)
